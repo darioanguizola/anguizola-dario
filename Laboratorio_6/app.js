ef8b476ca3b76c2ee37730ac6f02600ae8d0b6ca
Dario Anguizola <darioanguizola@gmail.com>
2024-05-15 04:38:58 -0500

    Update app.js

--- Laboratorio_6/app.js
+++ Laboratorio_6/app.js
@@ -1,21 +1,99 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('login-form');
+(() => {
+    const App = {
+      htmlElements: {
+        form: document.getElementById("login-form"),
+        usernameInput: document.getElementById('username'),
+        passwordInput: document.getElementById('password'),
+        
+
+      },
   
-    loginForm.addEventListener('submit', function(event) {
-      event.preventDefault();
+      init() {
+       
+        App.bindEvents();
+       
+       
+          
+      },
   
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
+    bindEvents() {
+        
+        App.htmlElements.form.addEventListener("submit", App.handlers.onSubmit);
+        window.addEventListener("load", App.handlers.onLoad);
+        //App.methods.verificationsesion();
+    
+        },
   
-      // Verificación de credenciales (simulada)
-      if (username === 'usuario' && password === 'contraseña') {
-        // Iniciar sesión
-        localStorage.setItem('isLoggedIn', true);
-        window.location.replace('home.html');
-      } else {
-        // Mostrar mensaje de error
-        alert('Credenciales incorrectas');
+    handlers: {
+        onSubmit(event) {
+        event.preventDefault();
+
+        App.methods.userVerification();
+        
+        },
+       /* onLoad()
+        {           
+            App.methods.verificationsesion();
+        }*/
+    },
+  
+      methods: {
+
+        leervariables(){
+        
+        
+        },
+
+        logout() {
+            localStorage.removeItem('isLoggedIn'); // Eliminar la sesión almacenada
+            window.location.replace('index.html'); // Redirigir al usuario a la página de inicio de sesión
+          },
+
+        verificationsesion()
+        {
+            let isLoggedIn = localStorage.getItem('isLoggedIn');
+            if (isLoggedIn != true) {
+                //window.location.replace('index.html');
+                App.methods.logout();
+               // alert("Sesión NO existente, redirigido a LOGIN");
+               // return; // Detiene la ejecución del código después de redirigir
+            }
+            else if (isLoggedIn === 'true') {
+                window.location.replace('home.html'); // Redirige al usuario a la página de inicio si la sesión ya está iniciada
+                alert("Sesión existe, redirigido a HOME.HTML");
+                return; // Detiene la ejecución del código después de redirigir
+        }
+          },
+
+        userVerification() {
+            let isLoggedIn = localStorage.getItem('isLoggedIn');
+
+            if (isLoggedIn != true) {
+                window.location.replace('index.html');
+                //App.methods.logout();
+                const usr = App.htmlElements.usernameInput.value;
+                const pass = App.htmlElements.passwordInput.value;
+                if (usr === 'admin' && pass === 'admin') {
+            // Iniciar sesión
+                    localStorage.setItem('isLoggedIn', true);
+                    window.location.replace('home.html');
+                    alert('Sesion iniciada');
+                }
+                else{
+                alert('Credenciales incorrectas');
+                }
+            }
+            else if (isLoggedIn === true){
+
+                alert('Seion ya fue iniciada');
+                window.location.replace('home.html');
+
+            }   
+                
+        }
       }
-    });
-  });
+    };
+  
+    App.init();
+  })();
   
